674b31ddd718d48b1f0f14f79c3a37b22c949e8e
Scheider <s.scheider@uu.nl>
2023-03-30 11:21:46 +0200

    update

--- statistics.py
+++ statistics.py
@@ -1,4 +1,5 @@
 import transforge as tf
+from ratiotheory import ratiotheory
 
 def test(complex_string):
-    return cct.parse(complex_string, *(tf.Source() for _ in range(10)))
+    return ratiotheory.parse(complex_string, *(tf.Source() for _ in range(10)))
