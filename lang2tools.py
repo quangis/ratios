909279a338fd11c744fe94d1032249a84357dcdd
Niels Steenbergen <n.steenbergen@uu.nl>
2023-04-19 12:43:47 +0200

    Explicitly provide concrete types for typeschemas.

--- lang2tools.py
+++ lang2tools.py
@@ -5,20 +5,36 @@ for the <https://github.com/quangis/ratios> project."""
 import json
 from rdflib.term import URIRef
 from transforge import Language
+from transforge.type import Type, TypeSchema, TypeInstance
+from transforge.expr import Operator
 from transforge.namespace import shorten
 
 from ratiotheory import ratiotheory  # type: ignore
 
 
-def lang2tools(lang: Language) -> dict:
+def lang2tools(lang: Language, **kwargs: tuple[TypeInstance, ...]) -> dict:
     """
     Convert the operators of a transforge language into a tool annotation 
     dictionary that APE understands.
     """
     functions: list[dict] = []
-    for op in lang.operators.values():
+
+    if kwargs:
+        operators = kwargs
+    else:
+        operators = {op: () for op in lang.operators.keys()}
+
+    for name, args in operators.items():
+        op: Operator = lang.operators[name]
+        t: Type = op.type
+        ti: TypeInstance
+        if args:
+            assert isinstance(t, TypeSchema)
+            ti = t.schema(*args)
+        else:
+            ti = t.instance()
         tool: URIRef = lang.uri(op)
-        inputs, output = op.type.instance().io()
+        inputs, output = ti.io()
         functions.append({
             'id': str(tool),
             'label': shorten(tool),
