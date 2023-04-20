76c4d06f12e60ad1914e717c997f07efa4db5df0
Niels Steenbergen <n.steenbergen@uu.nl>
2023-04-21 00:58:54 +0200

    Generate variants for type variables in operators.

--- lang2tools.py
+++ lang2tools.py
@@ -3,46 +3,54 @@ tools that can be passed to APE. At the time of writing, this is relevant only
 for the <https://github.com/quangis/ratios> project."""
 
 import json
-from rdflib.term import URIRef
 from transforge import Language
-from transforge.type import Type, TypeSchema, TypeInstance
-from transforge.expr import Operator
+from transforge.type import TypingError, TypeSchema, TypeInstance, Top, Bottom
 from transforge.namespace import shorten
+from itertools import product, chain
+from typing import Iterator, Iterable
 
 from ratiotheory import ratiotheory  # type: ignore
 
 
-def lang2tools(lang: Language, **kwargs: tuple[TypeInstance, ...]) -> dict:
-    """
-    Convert the operators of a transforge language into a tool annotation 
-    dictionary that APE understands.
-    """
+def variants(ts: TypeSchema, canon: set[TypeInstance])\
+        -> Iterator[TypeInstance]:
+    """Produce concrete variants of a function type schema, in such a way that 
+    the inputs and outputs are canonical types."""
+    for types in product(*(canon for _ in range(ts.n))):
+        try:
+            candidate = ts.schema(*types)
+        except TypingError:
+            continue
+        if all(t in canon for t in chain(*candidate.io())):
+            yield candidate
+
+
+def lang2tools(lang: Language) -> dict:
+    """Convert the operators of a transforge language into a tool annotation 
+    dictionary that APE understands."""
     functions: list[dict] = []
 
-    if kwargs:
-        operators = kwargs
-    else:
-        operators = {op: () for op in lang.operators.keys()}
+    top, bottom = Top(), Bottom()
+    canon: set[TypeInstance] = set(t for t in lang.canon
+        if top not in t and bottom not in t)
 
-    for name, args in operators.items():
-        op: Operator = lang.operators[name]
-        t: Type = op.type
-        ti: TypeInstance
-        if args:
-            assert isinstance(t, TypeSchema)
-            ti = t.schema(*args)
+    for name, op in lang.operators.items():
+        tis: Iterable[TypeInstance]
+        if isinstance(op.type, TypeSchema):
+            tis = variants(op.type, canon)
         else:
-            ti = t.instance()
-        tool: URIRef = lang.uri(op)
-        inputs, output = ti.io()
-        functions.append({
-            'id': str(tool),
-            'label': shorten(tool),
-            'taxonomyOperations': [tool],
-            'inputs': [{lang.namespace.Top: [lang.uri(input)]}
-                for input in inputs],
-            'outputs': [{lang.namespace.Top: [lang.uri(output)]}],
-        })
+            tis = (op.type.instance(),)
+        for i, ti in enumerate(tis):
+            tool = f"{lang.uri(op)}{i}"
+            inputs, output = ti.io()
+            functions.append({
+                'id': tool,
+                'label': shorten(tool),
+                'taxonomyOperations': [tool],
+                'inputs': [{lang.namespace.Top: [lang.uri(input)]}
+                    for input in inputs],
+                'outputs': [{lang.namespace.Top: [lang.uri(output)]}],
+            })
     return {'functions': functions}
 
 
